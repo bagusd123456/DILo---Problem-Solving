ce97fe00d993834b3c23057b44f4de65bd7d9aa8
bagusd123456 <41263511+bagusd123456@users.noreply.github.com>
2022-01-01 22:30:03 +0700

    Problem 2

---
+++ Assets/Script/CircleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    public float force = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(15f, 5f) * Time.deltaTime * force);
+    }
+}
