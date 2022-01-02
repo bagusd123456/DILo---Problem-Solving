65830eca6ca6e3f5aec11162ca67e6b896c2f861
bagusd123456 <41263511+bagusd123456@users.noreply.github.com>
2022-01-02 21:55:49 +0700

    Problem 3

--- Assets/Script/CircleController.cs
+++ Assets/Script/CircleController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CircleController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Vector2 direction;
 
     [SerializeField]
     public float force = 15;
@@ -12,11 +13,21 @@ public class CircleController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        //direction = new Vector2(15f, 5f);
+        direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        rb.AddForce(new Vector2(15f, 5f) * Time.deltaTime * force);
+        transform.Translate(direction * force, Space.World);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var contactPoint = collision.contacts[0].point;
+        Vector2 ballLocation = transform.position;
+        var inNormal = (ballLocation - contactPoint).normalized;
+        direction = Vector2.Reflect(direction, inNormal);
     }
 }
